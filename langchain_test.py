44496db855a1e000c1488c024ca5fe5701bcdd28
lpierrey <louispierrey10@gmail.com>
2025-02-07 10:57:00 +0100

    first commit

---
+++ langchain_test.py
@@ -0,0 +1,94 @@
+import getpass
+import os
+from langchain_mistralai import ChatMistralAI
+from langchain_chroma import Chroma
+import bs4
+from langchain import hub
+from langchain_mistralai import MistralAIEmbeddings
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_core.documents import Document
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langgraph.graph import START, StateGraph
+from typing_extensions import List, TypedDict
+from langgraph.graph import START, StateGraph
+from IPython.display import Image, display
+from langchain_openai import OpenAIEmbeddings
+from langchain_openai import ChatOpenAI
+from langchain_huggingface import HuggingFaceEmbeddings
+
+if not os.environ.get("OPENAI_API_KEY"):
+  os.environ["MISTRAL_API_KEY"] = getpass.getpass("Enter API key for Mistral: ")
+
+llm = ChatMistralAI(model="mistral-large-latest")
+
+embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-mpnet-base-v2")
+
+vector_store = Chroma(embedding_function=embeddings)
+
+
+# Only keep post title, headers, and content from the full HTML.
+bs4_strainer = bs4.SoupStrainer(class_=("post-title", "post-header", "post-content"))
+loader = WebBaseLoader(
+    web_paths=("https://lilianweng.github.io/posts/2023-06-23-agent/",),
+    bs_kwargs={"parse_only": bs4_strainer},
+)
+docs = loader.load()
+
+assert len(docs) == 1
+print(f"Total characters: {len(docs[0].page_content)}")
+
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=1000,  # chunk size (characters)
+    chunk_overlap=200,  # chunk overlap (characters)
+    add_start_index=True,  # track index in original document
+)
+all_splits = text_splitter.split_documents(docs)
+
+print(f"Split blog post into {len(all_splits)} sub-documents.")
+
+# print(type(all_splits))
+
+document_ids = vector_store.add_documents(documents=all_splits)
+
+print(document_ids[:3])
+
+
+prompt = hub.pull("rlm/rag-prompt")
+
+example_messages = prompt.invoke(
+    {"context": "(context goes here)", "question": "(question goes here)"}
+).to_messages()
+
+assert len(example_messages) == 1
+print(example_messages[0].content)
+
+
+
+class State(TypedDict):
+    question: str
+    context: List[Document]
+    answer: str
+    
+def retrieve(state: State):
+    retrieved_docs = vector_store.similarity_search(state["question"])
+    return {"context": retrieved_docs}
+
+
+def generate(state: State):
+        docs_content = "\n\n".join(doc.page_content for doc in state["context"])
+        messages = prompt.invoke({"question": state["question"], "context": docs_content})
+        response = llm.invoke(messages)
+        return {"answer": response.content}
+    
+
+graph_builder = StateGraph(State).add_sequence([retrieve, generate])
+graph_builder.add_edge(START, "retrieve")
+graph = graph_builder.compile()
+    
+
+display(Image(graph.get_graph().draw_mermaid_png()))
+    
+result = graph.invoke({"question": "What are the main types of memory ?"})
+
+print(f'Context: {result["context"]}\n\n')
+print(f'Answer: {result["answer"]}')
