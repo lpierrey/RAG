e47a72b7752e374c44e9db6d4cc687a3b0ce330b
lpierrey <louispierrey10@gmail.com>
2025-02-07 12:01:24 +0100

    enable PDF

--- langchain_test.py
+++ langchain_test.py
@@ -15,6 +15,10 @@ from IPython.display import Image, display
 from langchain_openai import OpenAIEmbeddings
 from langchain_openai import ChatOpenAI
 from langchain_huggingface import HuggingFaceEmbeddings
+from langchain.document_loaders import PyMuPDFLoader
+
+os.environ["LANGSMITH_TRACING"] = "true"
+os.environ["LANGSMITH_API_KEY"] = getpass.getpass()
 
 if not os.environ.get("OPENAI_API_KEY"):
   os.environ["MISTRAL_API_KEY"] = getpass.getpass("Enter API key for Mistral: ")
@@ -27,15 +31,18 @@ vector_store = Chroma(embedding_function=embeddings)
 
 
 # Only keep post title, headers, and content from the full HTML.
-bs4_strainer = bs4.SoupStrainer(class_=("post-title", "post-header", "post-content"))
-loader = WebBaseLoader(
-    web_paths=("https://lilianweng.github.io/posts/2023-06-23-agent/",),
-    bs_kwargs={"parse_only": bs4_strainer},
-)
+# bs4_strainer = bs4.SoupStrainer(class_=("post-title", "post-header", "post-content"))
+# loader = WebBaseLoader(
+#     web_paths=("https://lilianweng.github.io/posts/2023-06-23-agent/",),
+#     bs_kwargs={"parse_only": bs4_strainer},
+# )
+
+loader = PyMuPDFLoader("1706.03762v7.pdf")  #for PDF use
+
 docs = loader.load()
 
-assert len(docs) == 1
-print(f"Total characters: {len(docs[0].page_content)}")
+# assert len(docs) == 1
+# print(f"Total characters: {len(docs[0].page_content)}")
 
 text_splitter = RecursiveCharacterTextSplitter(
     chunk_size=1000,  # chunk size (characters)
@@ -49,6 +56,7 @@ print(f"Split blog post into {len(all_splits)} sub-documents.")
 # print(type(all_splits))
 
 document_ids = vector_store.add_documents(documents=all_splits)
+
 
 print(document_ids[:3])
 
@@ -88,7 +96,7 @@ graph = graph_builder.compile()
 
 display(Image(graph.get_graph().draw_mermaid_png()))
     
-result = graph.invoke({"question": "What are the main types of memory ?"})
+result = graph.invoke({"question": "What is Attention ?"})
 
 print(f'Context: {result["context"]}\n\n')
 print(f'Answer: {result["answer"]}')
